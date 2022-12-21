0de579212c2e8dde144648eaa5f883116119c2a7
detbin <116663162+detbin@users.noreply.github.com>
2022-12-21 23:58:09 +0100

    Add files via upload

---
+++ ejercicionumeros.js
@@ -0,0 +1,16 @@
+let alturacm=178
+let alturamt=1.78
+let peso=128.50
+let roundalt=Math.round(alturamt)
+console.log(roundalt)
+let roundpeso=(Math.round(peso)-1)
+console.log(roundpeso)
+maximo=false
+if ((Number.MAX_VALUE+1)==(Number.MAX_VALUE)){
+    maximo=true
+}else{
+    maximo=false
+}
+console.log(maximo)
+
+
